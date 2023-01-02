c113cd48b30edafa8ca31689a69d6dcf8b870b71
MohammedALassaf <82783342+MohammedALassaf@users.noreply.github.com>
2023-01-02 21:58:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/training/basics.kt
@@ -0,0 +1,33 @@
+package com.example.training
+
+fun main(){
+    // TODO: Assignment 1 What is the result of the following?
+    /*
+    * false && false -> false
+    * 10 % 3 -> 1
+    * 5 >= 5 -> true
+    * false || true -> true
+    * 5-3*2 -> -1
+    * 3 != 2 -> true
+    * (6+6) / 3 -> 4
+    * */
+    // TODO: Assignment 2
+    // Part 1
+    for(i in 0..100) {
+        if (i == 71) {
+            println("IT\'S OVER 70!!!")
+            break
+        } //else println(i)
+    }
+    // Part
+    var temperature = 35
+    while(temperature >10 ){
+        when{
+            temperature == 30-> println("It\'s Hot")
+            temperature ==20-> println("It's comfy")
+            temperature ==15 -> println("It's Cold!!!")
+        }
+            temperature--
+    }
+}
+
